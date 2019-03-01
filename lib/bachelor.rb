d98640450bc598056869d70d27d3ea3a5060d443
Samuel Guo <guosamuel1114@gmail.com>
2019-03-01 01:22:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   winner = " "
   data.each do |correct_season, contestants_info|
     if correct_season == season
-      constestants_info.each do |contestant|
+      contestants_info.each do |contestant|
         constestant.each do |category, personal_info|
           if personal_info = "Winner"
             winner = constestant["name"]
